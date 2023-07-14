4fb06b219824c61ff155d0d56bd43d26a375aec6
Jon Sadowski <jwsadowski@gmail.com>
2023-07-14 14:01:25 -0500

    eighth commit

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+document.addEventListener('mousemove', move);
+function move(e){
+    this.querySelectorAll('.move').forEach(layer =>{
+        const speed = layer.getAttribute('data-speed')
 
+        const x = (window.innerWidth - e.pageX*speed)/120;
+        const y = (window.innerWidth - e.pageY*speed)/120;
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    })
+}
 
 gsap.from('.logo', {opacity: 0, duration: 1, delay: 2, y:10})
 gsap.from('.navbar', 'nav_item', {opacity: 0, duration: 1, delay: 2.1, y:30, stagger: 0.2})
